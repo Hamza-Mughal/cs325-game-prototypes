9337874493942463d2e6e9fded3579f8701325bd
hamzamughal123 <lack4@hotmail.com>
2017-02-06 18:44:11 -0500

    test

--- Cowboy_Game/js/main.js
+++ Cowboy_Game/js/main.js
@@ -1,121 +1,109 @@
-window.onload = function() {
-    // You might want to start with a template that uses GameStates:
-    //     https://github.com/photonstorm/phaser/tree/v2.6.2/resources/Project%20Templates/Basic
-    
-    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
-    // You will need to change the fourth parameter to "new Phaser.Game()" from
-    // 'phaser-example' to 'game', which is the id of the HTML element where we
-    // want the game to go.
-    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
-    // You will need to change the paths you pass to "game.load.image()" or any other
-    // loading functions to reflect where you are putting the assets.
-    // All loading functions will typically all be found inside "preload()".
-    
-    "use strict";
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+// Create our 'main' state that will contain the game
+var mainState = {
+    preload: function() { 
+        // This function will be executed at the beginning     
+        // That's where we load the images and sounds 
+    },
 
-function preload() {
-	game.load.image('sky', 'assets/sky.png');
-    game.load.image('ground', 'assets/platform.png');
-    game.load.image('star', 'assets/star.png');
-    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
-}
+    create: function() { 
+        // This function is called after the preload function     
+        // Here we set up the game, display sprites, etc.  
+    },
 
-function create() {
-	    //  We're going to be using physics, so enable the Arcade Physics system
+    update: function() {
+        // This function is called 60 times per second    
+        // It contains the game's logic   
+    },
+};
+
+// Initialize Phaser, and create a 400px by 490px game
+var game = new Phaser.Game(400, 490);
+
+// Add the 'mainState' and call it 'main'
+game.state.add('main', mainState); 
+
+// Start the state to actually start the game
+game.state.start('main');
+preload: function() { 
+    // Load the bird sprite
+    game.load.image('bird', 'assets/bird.png'); 
+	game.load.image('pipe', 'assets/pipe.png');
+},
+
+create: function() { 
+    // Change the background color of the game to blue
+    game.stage.backgroundColor = '#71c5cf';
+
+    // Set the physics system
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    //  A simple background for our game
-    game.add.sprite(0, 0, 'sky');
+    // Display the bird at the position x=100 and y=245
+    this.bird = game.add.sprite(100, 245, 'bird');
 
-    //  The platforms group contains the ground and the 2 ledges we can jump on
-    platforms = game.add.group();
+    // Add physics to the bird
+    // Needed for: movements, gravity, collisions, etc.
+    game.physics.arcade.enable(this.bird);
 
-    //  We will enable physics for any object that is created in this group
-    platforms.enableBody = true;
+    // Add gravity to the bird to make it fall
+    this.bird.body.gravity.y = 1000;  
 
-    // Here we create the ground.
-    var ground = platforms.create(0, game.world.height - 64, 'ground');
+    // Call the 'jump' function when the spacekey is hit
+    var spaceKey = game.input.keyboard.addKey(
+                    Phaser.Keyboard.SPACEBAR);
+    spaceKey.onDown.add(this.jump, this);     
+	this.pipes = game.add.group(); 
+	this.timer = game.time.events.loop(1500, this.addRowOfPipes, this); 
+	this.score = 0;
+this.labelScore = game.add.text(20, 20, "0", 
+    { font: "30px Arial", fill: "#ffffff" });   
+},
 
-    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-    ground.scale.setTo(2, 2);
+update: function() {
+    // If the bird is out of the screen (too high or too low)
+    // Call the 'restartGame' function
+    if (this.bird.y < 0 || this.bird.y > 490)
+        this.restartGame();
+	game.physics.arcade.overlap(
+    this.bird, this.pipes, this.restartGame, null, this);
+},
+// Make the bird jump 
+jump: function() {
+    // Add a vertical velocity to the bird
+    this.bird.body.velocity.y = -350;
+},
 
-    //  This stops it from falling away when you jump on it
-    ground.body.immovable = true;
+// Restart the game
+restartGame: function() {
+    // Start the 'main' state, which restarts the game
+    game.state.start('main');
+},
+addOnePipe: function(x, y) {
+    // Create a pipe at the position x and y
+    var pipe = game.add.sprite(x, y, 'pipe');
 
-    //  Now let's create two ledges
-    var ledge = platforms.create(400, 400, 'ground');
+    // Add the pipe to our previously created group
+    this.pipes.add(pipe);
 
-    ledge.body.immovable = true;
+    // Enable physics on the pipe 
+    game.physics.arcade.enable(pipe);
 
-    ledge = platforms.create(-150, 250, 'ground');
+    // Add velocity to the pipe to make it move left
+    pipe.body.velocity.x = -200; 
 
-    ledge.body.immovable = true;
-	
-	var player;
-	 // The player and its settings
-    player = game.add.sprite(32, game.world.height - 150, 'dude');
+    // Automatically kill the pipe when it's no longer visible 
+    pipe.checkWorldBounds = true;
+    pipe.outOfBoundsKill = true;
+},
+addRowOfPipes: function() {
+    // Randomly pick a number between 1 and 5
+    // This will be the hole position
+    var hole = Math.floor(Math.random() * 5) + 1;
 
-    //  We need to enable physics on the player
-    game.physics.arcade.enable(player);
-
-    //  Player physics properties. Give the little guy a slight bounce.
-    player.body.bounce.y = 0.2;
-    player.body.gravity.y = 300;
-    player.body.collideWorldBounds = true;
-
-    //  Our two animations, walking left and right.
-    player.animations.add('left', [0, 1, 2, 3], 10, true);
-    player.animations.add('right', [5, 6, 7, 8], 10, true);
-	   stars = game.add.group();
-
-    stars.enableBody = true;
-
-    //  Here we'll create 12 of them evenly spaced apart
-    for (var i = 0; i < 12; i++)
-    {
-        //  Create a star inside of the 'stars' group
-        var star = stars.create(i * 70, 0, 'star');
-
-        //  Let gravity do its thing
-        star.body.gravity.y = 6;
-
-        //  This just gives each star a slightly random bounce value
-        star.body.bounce.y = 0.7 + Math.random() * 0.2;
-    }
-}
-
-function update() {
-	  var hitPlatform = game.physics.arcade.collide(player, platforms);
-	    //  Reset the players velocity (movement)
-    player.body.velocity.x = 0;
-
-    if (cursors.left.isDown)
-    {
-        //  Move to the left
-        player.body.velocity.x = -150;
-
-        player.animations.play('left');
-    }
-    else if (cursors.right.isDown)
-    {
-        //  Move to the right
-        player.body.velocity.x = 150;
-
-        player.animations.play('right');
-    }
-    else
-    {
-        //  Stand still
-        player.animations.stop();
-
-        player.frame = 4;
-    }
-
-    //  Allow the player to jump if they are touching the ground.
-    if (cursors.up.isDown && player.body.touching.down && hitPlatform)
-    {
-        player.body.velocity.y = -350;
-    }
-}
-};
+    // Add the 6 pipes 
+    // With one big hole at position 'hole' and 'hole + 1'
+    for (var i = 0; i < 8; i++)
+        if (i != hole && i != hole + 1) 
+            this.addOnePipe(400, i * 60 + 10);  
+this.score += 1;
+this.labelScore.text = this.score;  		
+},
