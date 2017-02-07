a4f58693e96c5d963d8463585d791801a9f71ef1
hamzamughal123 <lack4@hotmail.com>
2017-02-07 18:08:11 -0500

    up

--- Cowboy_Game/js/main.js
+++ Cowboy_Game/js/main.js
@@ -21,11 +21,14 @@ window.onload = function() {
 	var bullets;
 	var bulletTime = 0;
 	var fire;
+	
+	var enemies;
     function preload() {
         // Load an image and call it 'logo'.
         game.load.image( 'earthi', 'assets/mbe_earth.jpg' );
 		game.load.image('player', 'assets/cowboy.png');
 		game.load.image('bullet', 'assets/bulletbill.jpg');
+		game.load.image('enemy', 'assets/asteroid.jpg');
     }
     
     var bouncy;
@@ -42,16 +45,12 @@ window.onload = function() {
 		game.physics.enable(player,Phaser.Physics.ARCADE);
 		input = game.input.keyboard.createCursorKeys();
 		
-		bullets = game.add.group();
-		bullets.enableBody = true;
-		bullets.physicsBodyType = Phaser.Physics.ARCADE;
-		bullets.createMultiple(30, 'bullet');
-		bullets.setAll('anchor.x', 0.5);
-		bullets.setAll('anchor.y', 1);
-		bullets.setAll('outOfBoundsKill', true);
-		bullets.setAll('checkWorldBounds', true);
+		enemies = game.add.group();
+		enemies.enableBody = true;
+		enemies.physicsBodyType = Phaser.Physics.ARCADE;
 		
-		fire = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		createEnemies();
+		
     }
     
     function update() {
@@ -69,18 +68,22 @@ window.onload = function() {
 		if(input.right.isDown){
 			player.body.velocity.x = 350;
 		}
-		if(fire.isDown){
-			fireBullet();
-		}
     }
-	function fireBullet(){
-		if(game.time.now > bulletTime){
-			bullet = bullets.getFirstExists(false);
-			if(bullet){
-				bullet.reset(player.x + 14,player.y);
-				bullet.body.velocity.y = -400;
-				bulletTime = game.time.now + 200;
+	
+	function createEnemies(){
+		for(var y = 0; y < 4; y++){
+			for(var x = 0; x< 10; x++){
+				var enemy = enemies.create(x*48, y*50,'enemy');
+				enemy.anchor.setTo(0.5, 0.5);
 			}
 		}
+		enemies.x = 100;
+		enemies.y = 50;
+		
+		var tween = game.add.tween(enemies).to({x:200}, 2000,Phaser.Easing.Linear.None,true,0,1000,true);
+		tween.onLoop.add(descend,this);
+	}
+	function descend(){
+		enemies.y += 10;
 	}
 };
