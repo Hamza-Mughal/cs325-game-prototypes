133fd8c1a5a4396583774c39552b322a78e915f2
hamzamughal123 <lack4@hotmail.com>
2017-02-08 22:30:32 -0500

    test

--- Cowboy_Game/js/main.js
+++ Cowboy_Game/js/main.js
@@ -125,7 +125,7 @@ window.onload = function() {
 				enemy.anchor.setTo(0.5, 0.5);
 			}
 		}
-		enemies.x = 100;
+		enemies.x = 150;
 		enemies.y = 50;
 		
 		var tween = game.add.tween(enemies).to({x:200}, 2000,Phaser.Easing.Linear.None,true,0,1000,true);
