e08f641ea086596a586382026e98f4e420ba108c
hamzamughal123 <lack4@hotmail.com>
2017-02-15 19:46:44 -0500

    pl

--- HW2/js/main.js
+++ HW2/js/main.js
@@ -15,15 +15,17 @@ window.onload = function() {
     
     var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     var room;
-	
+	var player
     function preload() {
 		game.load.image('roomi', 'assets/room.jpg');
+		game.load.image('play', 'assets/gown.jpg');
     }
 	
     
     function create() {
 		// game.physics.enable(player,Phaser.Physics.ARCADE);
 		back = game.add.tileSprite(0,0,800,600,'roomi');
+		player = game.add.sprite(0, 300, 'play');
     }
     
     function update() {
