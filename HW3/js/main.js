50b5835062df27b9fd7427d1272ee35af48ccecc
hamzamughal123 <lack4@hotmail.com>
2017-02-23 00:05:42 -0500

    up

--- HW3/js/main.js
+++ HW3/js/main.js
@@ -21,7 +21,7 @@ var mainState = {
 	this.timer = game.time.events.loop(1500, this.addRowOfFires, this); 
 	
 	this.score = 0;
-	this.labelScore = game.add.text(20, 20, "0", { font: "30px Arial", fill: "#ffffff" });
+	this.labelScore = game.add.text(60, 60, "0", { font: "30px Arial", fill: "#000000" });
 
 	this.audi = game.sound.play('ricco');
 	this.audi.play();
