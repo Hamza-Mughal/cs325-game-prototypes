c4b57f41557838259cdb5f7962d5f43741183230
hamzamughal123 <lack4@hotmail.com>
2017-02-22 21:45:48 -0500

    u\pd

--- HW3/js/main.js
+++ HW3/js/main.js
@@ -1,6 +1,7 @@
 var mainState = {
     preload: function() { 
 	game.load.image('bravo' , 'assets/bravo.png');
+	game.load.image('fire', 'assets/fire.png');
     },
 
     create: function() { 
@@ -12,7 +13,11 @@ var mainState = {
 	this.bravo.body.gravity.y = 1000;  
 	var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 	spaceKey.onDown.add(this.jump, this);
-	this.input.onDown.add(this.jump, this);
+	this.input.onDown.add(this.jump, this); // mouse click
+	
+	this.fires = game.add.group();
+	
+	this.timer = game.time.events.loop(1500, this.addRowOfFires, this); 
     },
 
     update: function() {
@@ -29,6 +34,30 @@ var mainState = {
     game.state.start('main');
 	},
 	
+	addFire: function(x,y){
+		var fire = game.add.sprite(x,y, 'fire');
+		
+		this.fires.add(fire);
+		
+		game.physics.arcade.enable(fire);
+		
+		fire.body.velocity.x = -200;
+		
+		fire.checkWorldBounds = true;
+		fire.outOfBoundsKill = true;		
+	},
+	
+	addRowOfFires: function() {
+    // Randomly pick a number between 1 and 5
+    // This will be the hole position
+    var hole = Math.floor(Math.random() * 5) + 1;
+
+    // Add the 6 pipes 
+    // With one big hole at position 'hole' and 'hole + 1'
+    for (var i = 0; i < 8; i++)
+        if (i != hole && i != hole + 1) 
+            this.addFire(400, i * 60 + 10);   
+},
 };
 
 // Initialize Phaser, and create a 400px by 490px game
