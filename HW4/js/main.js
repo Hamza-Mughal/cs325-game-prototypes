ead28a063e3aded2eaa2b1003c9bf7630c27eead
hamzamughal123 <lack4@hotmail.com>
2017-02-27 17:03:15 -0500

    up

--- HW4/js/main.js
+++ HW4/js/main.js
@@ -92,7 +92,7 @@ this.timer1 = game.time.events.loop(2000, this.addBanana1, this);
 		banana.outOfBoundsKill = true;		
 	},
 	addBanana1: function(){
-		var banana = game.add.sprite(200,500, 'banana');
+		var banana = game.add.sprite(600,500, 'banana');
 		
 		this.Banana.add(banana);
 		game.physics.arcade.enable(banana);
