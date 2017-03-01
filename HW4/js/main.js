785112d55ec332ddefbf44d968da16cee661ca5a
hamzamughal123 <lack4@hotmail.com>
2017-03-01 17:11:36 -0500

    update

--- HW4/js/main.js
+++ HW4/js/main.js
@@ -37,8 +37,7 @@ var mainState = {
 	var weapon = game.input.keyboard.addKey(Phaser.Keyboard.W);
 	weapon.onDown.add(this.destroyBanana, this);
 
-	var weapon1 = game.input.keyboard.addKey(Phaser.Keyboard.S);
-	weapon.onDown.add(this.winGame, this); 	
+	var input = game.input.keyboard.createCursorKeys();
 	
 	this.audi = game.sound.play('ricco');
 	this.audi.play();
@@ -61,7 +60,7 @@ var mainState = {
 		}
 
 	if(this.score > 500){
-		if(weapon1.input.isDown){
+		if(this.input.down.isDown){
 		game.world.removeAll();
 		}
 	}		
