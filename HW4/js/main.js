cb438d4ca31b19fca5ee0964eb6ccfbefa9b69a0
hamzamughal123 <lack4@hotmail.com>
2017-03-01 17:54:03 -0500

    up

--- HW4/js/main.js
+++ HW4/js/main.js
@@ -26,7 +26,7 @@ var mainState = {
 	this.timer3 = game.time.events.loop(2600, this.addBanana2, this); 
 	this.score = 0;
 	this.labelScore = game.add.text(60, 60, "0", { font: "30px Arial", fill: "#000000" });
-	this.labelScore1 = game.add.text(60, 65, "500 pts to win!", { font: "30px Arial", fill: "#000000" });
+	this.labelScore1 = game.add.text(60, 90, "500 pts to win!", { font: "20px Arial", fill: "#000000" });
 	
 	
 	this.enemy = game.add.sprite(game.world.centerX+300, game.world.centerY-200, 'doctor');
