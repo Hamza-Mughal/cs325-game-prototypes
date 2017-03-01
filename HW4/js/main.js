b3489a938f6f2604110e2b09249f5901531ea77f
hamzamughal123 <lack4@hotmail.com>
2017-03-01 16:22:33 -0500

    i

--- HW4/js/main.js
+++ HW4/js/main.js
@@ -51,9 +51,7 @@ var mainState = {
 		else{
 			this.enemy.body.velocity.y = -(Math.random()*3000);
 			this.enemy.body.velocity.x = -(Math.random()*400);
-		}
-	if (this.bravo.angle < 20)
-		this.bird.angle += 1;	
+		}	
     },
 	
 	jump: function() {
