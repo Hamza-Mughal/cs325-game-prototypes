f9e4036580d427568654354c9c746c019a049151
hamzamughal123 <lack4@hotmail.com>
2017-03-08 20:36:09 -0500

    update

--- HW5/js/main.js
+++ HW5/js/main.js
@@ -109,8 +109,8 @@ window.onload = function() {
 		earth.tilePosition.y += 2;
 		player.body.velocity.x = 0;
 		player.body.velocity.y = 0;
-		shipTrail.x = player.x;
-		shipTrail.y = player.y-50;
+		shipTrail.x = player.x+20;
+		shipTrail.y = player.y+50;
 		if(input.left.isDown){
 			player.body.velocity.x = -300;
 		}
