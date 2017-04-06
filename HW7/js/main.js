6ccbd412f1f4233602965afa8b58ed74905514fb
Hamza-Mughal <hmughal2@masonlive.gmu.edu>
2017-04-05 23:31:48 -0400

    update

--- HW7/js/main.js
+++ HW7/js/main.js
@@ -21,7 +21,7 @@ var playGame = function(game){};
 playGame.prototype = {
 	preload: function(){
           game.load.image("road", "assets/road1.jpg");
-          game.load.image("car", "assets/car.png");
+          game.load.image("car", "assets/ncar.png");
           game.load.image("obstacle", "assets/cartooncar.png");
 	},
   	create: function(){
